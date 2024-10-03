d93a614dc505272f1e4f00b4de806eae8080be85
nonamie <53796817+mathfox@users.noreply.github.com>
2024-10-03 13:55:16 +0300

    avoid naming collisions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,10 @@
-export = Make;
-export as namespace Make;
+export = MakeJsx;
+export as namespace MakeJsx;
 
-declare namespace Make {
+declare namespace MakeJsx {
 	type Key = string | number;
 
-	type ReactChild = MakeElement;
+	type ReactChild = MakeJsxElement;
 
 	type ReactFragment =
 		| Map<Key, ReactNode>
@@ -15,32 +15,23 @@ declare namespace Make {
 
 	type JSXElementConstructor<TProps> = (props: TProps) => ReactNode; //| (new (props: P) => Component<any, any>);
 
-	interface MakeElement<
+	interface MakeJsxElement<
 		TProps = any,
-		TType extends string | JSXElementConstructor<any> =
-			| string
-			| JSXElementConstructor<any>,
+		TType extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>,
 	> {
 		type: TType;
 		props: TProps;
 		key: string | undefined;
 	}
 
-	type InferEnumNames<TValue> = TValue extends EnumItem
-		? TValue["Name"]
-		: never;
+	type InferEnumNames<TValue> = TValue extends EnumItem ? TValue["Name"] : never;
 
 	export type InstanceAttributes<TInstance extends Instance> = {
-		[TKey in Exclude<WritablePropertyNames<TInstance>, "Name">]?:
-			| TInstance[TKey]
-			| InferEnumNames<TInstance[TKey]>;
+		[TKey in Exclude<WritablePropertyNames<TInstance>, "Name">]?: TInstance[TKey] | InferEnumNames<TInstance[TKey]>;
 	};
 
 	export type InstanceEvent<TInstance extends Instance> = {
-		[TKey in ExtractKeys<
-			TInstance,
-			RBXScriptSignal
-		>]?: TInstance[TKey] extends RBXScriptSignal<infer TCallback>
+		[TKey in ExtractKeys<TInstance, RBXScriptSignal>]?: TInstance[TKey] extends RBXScriptSignal<infer TCallback>
 			? (rbx: TInstance, ...args: Parameters<TCallback>) => void
 			: never;
 	};
@@ -49,35 +40,24 @@ declare namespace Make {
 		[TKey in InstancePropertyNames<TInstance>]?: (rbx: TInstance) => void;
 	};
 
-	export type InstanceProps<TInstance extends Instance> =
-		InstanceAttributes<TInstance> & {
-			Event?: InstanceEvent<TInstance>;
-			Change?: InstanceChangeEvent<TInstance>;
-			Tag?: string;
-		};
+	export type InstanceProps<TInstance extends Instance> = InstanceAttributes<TInstance> & {
+		Event?: InstanceEvent<TInstance>;
+		Change?: InstanceChangeEvent<TInstance>;
+		Tag?: string;
+	};
 
-	function createElement<TProps extends {}>(
-		type: string,
-		props?: TProps | undefined,
-		...children: []
-	): Instance;
+	function createElement<TProps extends {}>(type: string, props?: TProps | undefined, ...children: []): Instance;
 
-	function dom<TProps extends {}>(
-		type: string,
-		props?: TProps | undefined,
-		...children: []
-	): Instance;
+	function dom<TProps extends {}>(type: string, props?: TProps | undefined, ...children: []): Instance;
 }
 
 declare global {
 	namespace JSX {
-		//interface Element extends Make.MakeElement<any, any> {}
+		//interface Element extends MakeJsx.MakeJsxElement<any, any> {}
 		type Element = Instance;
 
 		type IntrinsicElements = {
-			[TKey in keyof Instances as Uncapitalize<TKey>]: Make.InstanceProps<
-				Instances[TKey]
-			>;
+			[TKey in keyof Instances as Uncapitalize<TKey>]: MakeJsx.InstanceProps<Instances[TKey]>;
 		};
 	}
 }
